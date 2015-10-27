c8c4163774258621ee04d9b013d28c6e89fc03b7
Jacob Piazza <jpiazza@ggc.edu>
2015-10-27 16:26:30 -0400

    Game class structure (without code)

--- ZombieMan Game/src/Game.java
+++ ZombieMan Game/src/Game.java
@@ -1,12 +1,45 @@
 package src;
 
+import java.util.ArrayList;
+
 public class Game
 {
 
+	Room currentRoom;
+	ArrayList<Room> roomList;
+	
+	public void newGame()
+	{
+		
+	}
+	
+	public void loadState()
+	{
+		
+	}
+	
+	public void saveState()
+	{
+		
+	}
+	
+	public void moveToRoom(Room r)
+	{
+		
+	}
+	
+	public void startEncounter(int eValue)
+	{
+		
+	}
+	
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		System.out.println("ZOMBIEMAN");
+		System.out.println("\nMAIN MENU (Select an option):");
+		System.out.println("1. Start a new game");
+		System.out.println("2. Load a saved game");
+		
 	}
 
 }
