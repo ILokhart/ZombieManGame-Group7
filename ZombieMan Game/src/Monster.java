12bc2414a877701f7a40c1e9398bd3af368a1009
Bhavan Patel <bpatel4@ggc.edu>
2015-11-04 10:03:54 -0500

    added constructor

--- ZombieMan Game/src/Monster.java
+++ ZombieMan Game/src/Monster.java
@@ -2,5 +2,25 @@ package src;
 
 public class Monster
 {
-	private String name;
+	private String MonsterName;
+	private String Clue; 
+	private String Description; 
+	private double ChanceToHit; 
+	private int HitPoints;
+
+
+public Monster (String MonsterName, String Clue, String Description,
+		             double ChanceToHit, int HitPoints)
+
+}
+
+
+
+
+
+
+
+
+
+  }
 }
