24ac8bde6433fa74ec5eb99544d31b01e5d869ed
Jonathan Mayran <Jonathan@Jonathans-MacBook-Air.local>
2015-11-09 10:17:18 -0500

    Inventory and Player update

--- ZombieMan Game/src/Inventory.java
+++ ZombieMan Game/src/Inventory.java
@@ -1,6 +1,18 @@
 package src;
 
+import java.util.ArrayList;
+
 public class Inventory
 {
-
+	private int solvedPuzzles;
+	ArrayList<String> clueList = new ArrayList<String>();
+	
+	public int getSolvedPuzzles()
+	{
+		return solvedPuzzles;
+	}
+	public void addSolvedPuzzles()
+	{
+		this.solvedPuzzles += 1;
+	}
 }
--- ZombieMan Game/src/Player.java
+++ ZombieMan Game/src/Player.java
@@ -1,7 +1,36 @@
 package src;
 
+import java.util.ArrayList;
+
+/**
+ * @author Jonathan Mayran
+ *
+ */
 public class Player
 {
 	private String name;
+	private static int minusLives;
+	private static int playerHP;
+	private final int baseCTH = 30;
+	private Inventory playerInventory;
+	private int playerCTH = baseCTH + (playerInventory.getSolvedPuzzles() * 10);
+	
+	
+	public String setName()
+	{
+		return name;
+	}
+	
+	public int setMinusLives()
+	{
+		return minusLives--;
+	}
+	
+	public int setPlayerHP()
+	{
+		return playerHP++;
+	}
+	
+
 
 }
